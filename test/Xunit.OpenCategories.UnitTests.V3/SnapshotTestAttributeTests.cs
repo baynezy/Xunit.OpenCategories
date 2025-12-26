71fbc40393249d5fa2e25ee4d79151fae4d8fa38
Simon Baynes <baynezy@gmail.com>
2025-12-26 14:33:06 +0000

    Fix inconsistencies between V2 and V3
    Closes #202

---
+++ test/Xunit.OpenCategories.UnitTests.V3/SnapshotTestAttributeTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit.OpenCategories.V3;
+
+namespace Xunit.OpenCategories.UnitTests.V3;
+
+public class SnapshotTestAttributeTests : CategoryOnlyTests<SnapshotTestAttribute>
+{
+    [Fact]
+    [SnapshotTest]
+    public void SnapshotTest()
+    {
+        var testMethod = typeof(SnapshotTestAttributeTests).GetMethod(nameof(SnapshotTest));
+        testMethod.Should()
+            .BeDecoratedWith<FactAttribute>()
+            .And.BeDecoratedWith<SnapshotTestAttribute>();
+    }
+
+    protected override string AttributeCategory => "SnapshotTest";
+}
---
+++ test/Xunit.OpenCategories.UnitTests/ServicesAttributeTests.cs
@@ -0,0 +1,46 @@
+﻿namespace Xunit.OpenCategories.UnitTests;
+
+public class ServicesAttributeTests
+{
+    [Fact]
+    [Services("Service A")]
+    public void ServicesWithSingleName()
+    {
+        var testMethod = typeof(ServicesAttributeTests).GetMethod(nameof(ServicesWithSingleName));
+        testMethod.Should()
+            .BeDecoratedWith<FactAttribute>()
+            .And.BeDecoratedWith<ServicesAttribute>()
+            .Which.ServiceNames.Should()
+            .Contain("Service A")
+            .And.HaveCount(1);
+    }
+
+    [Fact]
+    [Services("Service A", "Service B")]
+    public void ServicesWithTwoNames()
+    {
+        var testMethod = typeof(ServicesAttributeTests).GetMethod(nameof(ServicesWithTwoNames));
+        testMethod.Should()
+            .BeDecoratedWith<FactAttribute>()
+            .And.BeDecoratedWith<ServicesAttribute>()
+            .Which.ServiceNames.Should()
+            .Contain("Service A")
+            .And.Contain("Service B")
+            .And.HaveCount(2);
+    }
+
+    [Fact]
+    [Services("Service A", "Service B", "Service C")]
+    public void ServicesWithMultipleNames()
+    {
+        var testMethod = typeof(ServicesAttributeTests).GetMethod(nameof(ServicesWithMultipleNames));
+        testMethod.Should()
+            .BeDecoratedWith<FactAttribute>()
+            .And.BeDecoratedWith<ServicesAttribute>()
+            .Which.ServiceNames.Should()
+            .Contain("Service A")
+            .And.Contain("Service B")
+            .And.Contain("Service C")
+            .And.HaveCount(3);
+    }
+}
